a22fd111903a07444aaac482c44627c44661aa0f
201 <201@academicos.cesde.edu.co>
2022-10-04 11:26:04 -0500

    Se crea back de la app

--- src/main/java/com/tcc/apicuartostcc/controladores/MercanciaControlador.java
+++ src/main/java/com/tcc/apicuartostcc/controladores/MercanciaControlador.java
@@ -1,11 +1,48 @@
 package com.tcc.apicuartostcc.controladores;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.tcc.apicuartostcc.entidades.Mercancia;
+import com.tcc.apicuartostcc.entidades.Zona;
+import com.tcc.apicuartostcc.servicios.implementaciones.MercanciaServicioImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api/tcc/mercancias")
 
 public class MercanciaControlador {
 
+    @Autowired
+    MercanciaServicioImp mercanciaServicio;
 
+    @PostMapping
+    public ResponseEntity<?> registrar(@RequestBody Mercancia mercancia){
+
+        try{
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(mercanciaServicio.registrar(mercancia));
+        }catch(Exception error){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("{mensaje:Revise su peticion }");
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> buscarTodos(){
+        try{
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(mercanciaServicio.bsucarTodos());
+        }catch(Exception error){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("{mensaje: Datos no encontrados }");
+        }
+    }
+    
 
 }
